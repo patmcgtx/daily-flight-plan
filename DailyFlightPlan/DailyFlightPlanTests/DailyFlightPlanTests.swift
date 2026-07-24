ee63aa5316e40b3747d4c5f3b4b564625b1ac42d
patmcgtx <57506933+patmcgtx@users.noreply.github.com>
2026-07-24 08:42:04 -0500

    Initial Xcode skeleton project

---
+++ DailyFlightPlan/DailyFlightPlanTests/DailyFlightPlanTests.swift
@@ -0,0 +1,18 @@
+//
+//  DailyFlightPlanTests.swift
+//  DailyFlightPlanTests
+//
+//  Created by Patrick McGonigle on 7/24/26.
+//
+
+import Testing
+
+struct DailyFlightPlanTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+        // Swift Testing Documentation
+        // https://developer.apple.com/documentation/testing
+    }
+
+}
